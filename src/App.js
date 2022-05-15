47b465440d23c8c5679b337359df241c8c8b9ecd
keshavsachin <kesavansachin2000@gmail.com>
2022-05-15 20:23:39 +0530

    first react

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function Addition() {
+    let value1=190
+    return(
+      <div className='App'>
+        <h1>hello from addition</h1>
+        <h1>{value1}</h1>
+
+      </div>
+    );
+  }
+  let value=30
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+     <h1>Hello World</h1>
+     <h1>{value}</h1>
+     <Addition/>
+   </div>
   );
 }
 
