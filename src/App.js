77b7e3efb968e0cc212f1d94cd24eea8c29f8d49
keshavsachin <kesavansachin2000@gmail.com>
2022-05-18 12:07:30 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
 import './Input'
 import Input from './Input';
+import './Input';
+import './Input1.css';
 
 function App() {
+        
 
-  function Addition() {
-    let value1=190
-    return(
-      <div className='App'>
-        <h1>hello from addition</h1>
-        <h1>{value1}</h1>
+        return(
+          <div className='Input1'>
+          <div className='headercont'>
+            <h1>Header</h1>
+          </div>
+          <div className='container'>
+           <div className='navcont'>
+             <h2>Home</h2>
+             <h2>Explore</h2>
+             <h2>Shorts</h2>
+             <h2>Subscription</h2>
+             <h2>Library</h2>
+             <h2>History</h2>
+             <h2>Watch later</h2>
+             <h2>Liked videos</h2>
+           </div>
+            <div className='contentandfooter'>
+              <div className='content'>
+                <div className='videos'>tamil songs</div>
+                <div className='videos'>sachin tendulkar speech</div>
+                <div className='videos'>cricket videos</div>
+                <div className='videos'>Ben Lionel Scott</div>
+            
+              </div>
+              <div className='footer'>
+                <h2>Downloads</h2>
+                <h2>Favourites</h2>
+                <h2>Your Account</h2>
+              </div>
+          </div>
+          </div>
+            <Input/>
+          </div>
+      
+        )
+    }
+   
 
-      </div>
-    );
-  }
-  let value=30
+ 
 
-  return (
-   <div className='App'>
-     <h1>Hello World</h1>
-     <h1>{value}</h1>
-     <Addition/>
-     <Input/>
-   </div>
-  );
-}
 
 
 export default App;
--- src/Input.js
+++ src/Input.js
@@ -3,11 +3,7 @@ import React from 'react'
 function Input() {
   return (
     <div>
-        <input placeholder='name'/>
-        <input placeholder='email'/>
-        <input placeholder='password'/>
-        <button>submit</button>
-
+        
     </div>
   )
 }
